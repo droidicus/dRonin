9fa3a83593347b4a2265b813d6800197f684db59
droidicus <droidicus@dr.com>
2016-02-27 19:28:24 -0500

    stage 1

--- flight/Modules/Autotune/autotune.c
+++ flight/Modules/Autotune/autotune.c
@@ -55,11 +55,8 @@
 #include "misc_math.h"
 
 // Private constants
-#define STACK_SIZE_BYTES 1340
+#define STACK_SIZE_BYTES 3340
 #define TASK_PRIORITY PIOS_THREAD_PRIO_NORMAL
-
-#define AF_NUMX 13
-#define AF_NUMP 43
 
 // Private types
 enum AUTOTUNE_STATE { AT_INIT, AT_START, AT_RUN, AT_FINISHED, AT_WAITING };
@@ -67,7 +64,7 @@ enum AUTOTUNE_STATE { AT_INIT, AT_START, AT_RUN, AT_FINISHED, AT_WAITING };
 struct at_queued_data {
 	float y[3];		/* Gyro measurements */
 	float u[3];		/* Actuator desired */
-	float throttle;	/* Throttle desired */
+	float thrust;	/* thrust desired */
 
 	float dT;	/* From Gyro */
 };
@@ -77,7 +74,7 @@ static struct pios_thread *taskHandle;
 static bool module_enabled;
 static circ_queue_t at_queue;
 static volatile uint32_t at_points_spilled;
-static uint32_t throttle_accumulator;
+static uint32_t thrust_accumulator;
 
 // Private functions
 static void AutotuneTask(void *parameters);
@@ -169,7 +166,7 @@ static void at_new_gyro_data(UAVObjEvent * ev, void *ctx, void *obj, int len) {
 	q_item->u[1] = actuators.Pitch;
 	q_item->u[2] = actuators.Yaw;
 
-	q_item->throttle = actuators.Thrust;
+	q_item->thrust = actuators.Thrust;
 
 	if (circ_queue_advance_write(at_queue) != 0) {
 		last_sample_unpushed = true;
@@ -179,27 +176,15 @@ static void at_new_gyro_data(UAVObjEvent * ev, void *ctx, void *obj, int len) {
 }
 
 static void UpdateSystemIdent(const float *X, const float *noise,
-		float dT_s, uint32_t predicts, uint32_t spills, float hover_throttle) {
+		float dT_s, uint32_t predicts, uint32_t spills, float hover_thrust) {
 	SystemIdentData system_ident;
-	system_ident.Beta[SYSTEMIDENT_BETA_ROLL]    = X[6];
-	system_ident.Beta[SYSTEMIDENT_BETA_PITCH]   = X[7];
-	system_ident.Beta[SYSTEMIDENT_BETA_YAW]     = X[8];
-	system_ident.Bias[SYSTEMIDENT_BIAS_ROLL]    = X[10];
-	system_ident.Bias[SYSTEMIDENT_BIAS_PITCH]   = X[11];
-	system_ident.Bias[SYSTEMIDENT_BIAS_YAW]     = X[12];
-	system_ident.Tau                            = X[9];
 
-	if (noise) {
-		system_ident.Noise[SYSTEMIDENT_NOISE_ROLL]  = noise[0];
-		system_ident.Noise[SYSTEMIDENT_NOISE_PITCH] = noise[1];
-		system_ident.Noise[SYSTEMIDENT_NOISE_YAW]   = noise[2];
-	}
 	system_ident.Period = dT_s * 1000.0f;
 
 	system_ident.NumAfPredicts = predicts;
 	system_ident.NumSpilledPts = spills;
 
-	system_ident.HoverThrottle = hover_throttle;
+	system_ident.Hoverthrust = hover_thrust;
 
 	SystemIdentSet(&system_ident);
 }
@@ -250,10 +235,6 @@ static void AutotuneTask(void *parameters)
 	enum AUTOTUNE_STATE state = AT_INIT;
 
 	uint32_t last_update_time = PIOS_Thread_Systime();
-
-	float X[AF_NUMX] = {0};
-	float P[AF_NUMP] = {0};
-	float noise[3] = {0};
 
 	af_init(X,P);
 
@@ -335,7 +316,7 @@ static void AutotuneTask(void *parameters)
 					update_counter = 0;
 					at_points_spilled = 0;
 
-					throttle_accumulator = 0;
+					thrust_accumulator = 0;
 
 					state = AT_RUN;
 					last_update_time = PIOS_Thread_Systime();
@@ -371,21 +352,21 @@ static void AutotuneTask(void *parameters)
 						dT_s = pt->dT;
 					}
 
-					af_predict(X, P, pt->u, pt->y, dT_s, pt->throttle);
+					af_predict(X, P, pt->u, pt->y, dT_s, pt->thrust);
 
 					for (uint32_t i = 0; i < 3; i++) {
 						const float NOISE_ALPHA = 0.9997f;  // 10 second time constant at 300 Hz
 						noise[i] = NOISE_ALPHA * noise[i] + (1-NOISE_ALPHA) * (pt->y[i] - X[i]) * (pt->y[i] - X[i]);
 					}
 
-					//This will work up to 8kHz with an 89% throttle position before overflow
-					throttle_accumulator += 10000 * pt->throttle;
+					//This will work up to 8kHz with an 89% thrust position before overflow
+					thrust_accumulator += 10000 * pt->thrust;
 
 					// Update uavo every 256 cycles to avoid
 					// telemetry spam
 					if (!((update_counter++) & 0xff)) {
-						float hover_throttle = ((float)(throttle_accumulator/update_counter))/10000.0f;
-						UpdateSystemIdent(X, noise, dT_s, update_counter, at_points_spilled, hover_throttle);
+						float hover_thrust = ((float)(thrust_accumulator/update_counter))/10000.0f;
+						UpdateSystemIdent(X, noise, dT_s, update_counter, at_points_spilled, hover_thrust);
 					}
 
 					/* Free the buffer containing an AT point */
@@ -403,8 +384,8 @@ static void AutotuneTask(void *parameters)
 
 				// Wait until disarmed and landed before saving the settings
 
-				float hover_throttle = ((float)(throttle_accumulator/update_counter))/10000.0f;
-				UpdateSystemIdent(X, noise, 0, update_counter, at_points_spilled, hover_throttle);
+				float hover_thrust = ((float)(thrust_accumulator/update_counter))/10000.0f;
+				UpdateSystemIdent(X, noise, 0, update_counter, at_points_spilled, hover_thrust);
 
 				save_needed = true;
 				state = AT_WAITING;
@@ -436,202 +417,7 @@ static void AutotuneTask(void *parameters)
  */
 __attribute__((always_inline)) static inline void af_predict(float X[AF_NUMX], float P[AF_NUMP], const float u_in[3], const float gyro[3], const float dT_s, const float t_in)
 {
-	const float Ts = dT_s;
-	const float Tsq = Ts * Ts;
-	const float Tsq3 = Tsq * Ts;
-	const float Tsq4 = Tsq * Tsq;
 
-	// for convenience and clarity code below uses the named versions of
-	// the state variables
-	float w1 = X[0];           // roll rate estimate
-	float w2 = X[1];           // pitch rate estimate
-	float w3 = X[2];           // yaw rate estimate
-	float u1 = X[3];           // scaled roll torque
-	float u2 = X[4];           // scaled pitch torque
-	float u3 = X[5];           // scaled yaw torque
-	const float e_b1 = expapprox(X[6]);   // roll torque scale
-	const float b1 = X[6];
-	const float e_b2 = expapprox(X[7]);   // pitch torque scale
-	const float b2 = X[7];
-	const float e_b3 = expapprox(X[8]);   // yaw torque scale
-	const float b3 = X[8];
-	const float e_tau = expapprox(X[9]); // time response of the motors
-	const float tau = X[9];
-	const float bias1 = X[10];        // bias in the roll torque
-	const float bias2 = X[11];       // bias in the pitch torque
-	const float bias3 = X[12];       // bias in the yaw torque
-
-	// inputs to the system (roll, pitch, yaw)
-	const float u1_in = 4*t_in*u_in[0];
-	const float u2_in = 4*t_in*u_in[1];
-	const float u3_in = 4*t_in*u_in[2];
-
-	// measurements from gyro
-	const float gyro_x = gyro[0];
-	const float gyro_y = gyro[1];
-	const float gyro_z = gyro[2];
-
-	// update named variables because we want to use predicted
-	// values below
-	w1 = X[0] = w1 - Ts*bias1*e_b1 + Ts*u1*e_b1;
-	w2 = X[1] = w2 - Ts*bias2*e_b2 + Ts*u2*e_b2;
-	w3 = X[2] = w3 - Ts*bias3*e_b3 + Ts*u3*e_b3;
-	u1 = X[3] = (Ts*u1_in)/(Ts + e_tau) + (u1*e_tau)/(Ts + e_tau);
-	u2 = X[4] = (Ts*u2_in)/(Ts + e_tau) + (u2*e_tau)/(Ts + e_tau);
-	u3 = X[5] = (Ts*u3_in)/(Ts + e_tau) + (u3*e_tau)/(Ts + e_tau);
-    // X[6] to X[12] unchanged
-
-	/**** filter parameters ****/
-	const float q_w = 1e-3f;
-	const float q_ud = 1e-3f;
-	const float q_B = 1e-6f;
-	const float q_tau = 1e-6f;
-	const float q_bias = 1e-19f;
-	const float s_a = 150.0f; // expected gyro measurment noise
-
-	const float Q[AF_NUMX] = {q_w, q_w, q_w, q_ud, q_ud, q_ud, q_B, q_B, q_B, q_tau, q_bias, q_bias, q_bias};
-
-	float D[AF_NUMP];
-	for (uint32_t i = 0; i < AF_NUMP; i++)
-        D[i] = P[i];
-
-    const float e_tau2    = e_tau * e_tau;
-    const float e_tau3    = e_tau * e_tau2;
-    const float e_tau4    = e_tau2 * e_tau2;
-    const float Ts_e_tau2 = (Ts + e_tau) * (Ts + e_tau);
-    const float Ts_e_tau4 = Ts_e_tau2 * Ts_e_tau2;
-
-	// covariance propagation - D is stored copy of covariance
-	P[0] = D[0] + Q[0] + 2*Ts*e_b1*(D[3] - D[28] - D[9]*bias1 + D[9]*u1) + Tsq*(e_b1*e_b1)*(D[4] - 2*D[29] + D[32] - 2*D[10]*bias1 + 2*D[30]*bias1 + 2*D[10]*u1 - 2*D[30]*u1 + D[11]*(bias1*bias1) + D[11]*(u1*u1) - 2*D[11]*bias1*u1);
-	P[1] = D[1] + Q[1] + 2*Ts*e_b2*(D[5] - D[33] - D[12]*bias2 + D[12]*u2) + Tsq*(e_b2*e_b2)*(D[6] - 2*D[34] + D[37] - 2*D[13]*bias2 + 2*D[35]*bias2 + 2*D[13]*u2 - 2*D[35]*u2 + D[14]*(bias2*bias2) + D[14]*(u2*u2) - 2*D[14]*bias2*u2);
-	P[2] = D[2] + Q[2] + 2*Ts*e_b3*(D[7] - D[38] - D[15]*bias3 + D[15]*u3) + Tsq*(e_b3*e_b3)*(D[8] - 2*D[39] + D[42] - 2*D[16]*bias3 + 2*D[40]*bias3 + 2*D[16]*u3 - 2*D[40]*u3 + D[17]*(bias3*bias3) + D[17]*(u3*u3) - 2*D[17]*bias3*u3);
-	P[3] = (D[3]*(e_tau2 + Ts*e_tau) + Ts*e_b1*e_tau2*(D[4] - D[29]) + Tsq*e_b1*e_tau*(D[4] - D[29]) + D[18]*Ts*e_tau*(u1 - u1_in) + D[10]*e_b1*(u1*(Ts*e_tau2 + Tsq*e_tau) - bias1*(Ts*e_tau2 + Tsq*e_tau)) + D[21]*Tsq*e_b1*e_tau*(u1 - u1_in) + D[31]*Tsq*e_b1*e_tau*(u1_in - u1) + D[24]*Tsq*e_b1*e_tau*(u1*(u1 - bias1) + u1_in*(bias1 - u1)))/Ts_e_tau2;
-	P[4] = (Q[3]*Tsq4 + e_tau4*(D[4] + Q[3]) + 2*Ts*e_tau3*(D[4] + 2*Q[3]) + 4*Q[3]*Tsq3*e_tau + Tsq*e_tau2*(D[4] + 6*Q[3] + u1*(D[27]*u1 + 2*D[21]) + u1_in*(D[27]*u1_in - 2*D[21])) + 2*D[21]*Ts*e_tau3*(u1 - u1_in) - 2*D[27]*Tsq*u1*u1_in*e_tau2)/Ts_e_tau4;
-	P[5] = (D[5]*(e_tau2 + Ts*e_tau) + Ts*e_b2*e_tau2*(D[6] - D[34]) + Tsq*e_b2*e_tau*(D[6] - D[34]) + D[19]*Ts*e_tau*(u2 - u2_in) + D[13]*e_b2*(u2*(Ts*e_tau2 + Tsq*e_tau) - bias2*(Ts*e_tau2 + Tsq*e_tau)) + D[22]*Tsq*e_b2*e_tau*(u2 - u2_in) + D[36]*Tsq*e_b2*e_tau*(u2_in - u2) + D[25]*Tsq*e_b2*e_tau*(u2*(u2 - bias2) + u2_in*(bias2 - u2)))/Ts_e_tau2;
-	P[6] = (Q[4]*Tsq4 + e_tau4*(D[6] + Q[4]) + 2*Ts*e_tau3*(D[6] + 2*Q[4]) + 4*Q[4]*Tsq3*e_tau + Tsq*e_tau2*(D[6] + 6*Q[4] + u2*(D[27]*u2 + 2*D[22]) + u2_in*(D[27]*u2_in - 2*D[22])) + 2*D[22]*Ts*e_tau3*(u2 - u2_in) - 2*D[27]*Tsq*u2*u2_in*e_tau2)/Ts_e_tau4;
-	P[7] = (D[7]*(e_tau2 + Ts*e_tau) + Ts*e_b3*e_tau2*(D[8] - D[39]) + Tsq*e_b3*e_tau*(D[8] - D[39]) + D[20]*Ts*e_tau*(u3 - u3_in) + D[16]*e_b3*(u3*(Ts*e_tau2 + Tsq*e_tau) - bias3*(Ts*e_tau2 + Tsq*e_tau)) + D[23]*Tsq*e_b3*e_tau*(u3 - u3_in) + D[41]*Tsq*e_b3*e_tau*(u3_in - u3) + D[26]*Tsq*e_b3*e_tau*(u3*(u3 - bias3) + u3_in*(bias3 - u3)))/Ts_e_tau2;
-	P[8] = (Q[5]*Tsq4 + e_tau4*(D[8] + Q[5]) + 2*Ts*e_tau3*(D[8] + 2*Q[5]) + 4*Q[5]*Tsq3*e_tau + Tsq*e_tau2*(D[8] + 6*Q[5] + u3*(D[27]*u3 + 2*D[23]) + u3_in*(D[27]*u3_in - 2*D[23])) + 2*D[23]*Ts*e_tau3*(u3 - u3_in) - 2*D[27]*Tsq*u3*u3_in*e_tau2)/Ts_e_tau4;
-	P[9] = D[9] - Ts*e_b1*(D[30] - D[10] + D[11]*(bias1 - u1));
-	P[10] = (D[10]*(Ts + e_tau) + D[24]*Ts*(u1 - u1_in))*(e_tau/Ts_e_tau2);
-	P[11] = D[11] + Q[6];
-	P[12] = D[12] - Ts*e_b2*(D[35] - D[13] + D[14]*(bias2 - u2));
-	P[13] = (D[13]*(Ts + e_tau) + D[25]*Ts*(u2 - u2_in))*(e_tau/Ts_e_tau2);
-	P[14] = D[14] + Q[7];
-	P[15] = D[15] - Ts*e_b3*(D[40] - D[16] + D[17]*(bias3 - u3));
-	P[16] = (D[16]*(Ts + e_tau) + D[26]*Ts*(u3 - u3_in))*(e_tau/Ts_e_tau2);
-	P[17] = D[17] + Q[8];
-	P[18] = D[18] - Ts*e_b1*(D[31] - D[21] + D[24]*(bias1 - u1));
-	P[19] = D[19] - Ts*e_b2*(D[36] - D[22] + D[25]*(bias2 - u2));
-	P[20] = D[20] - Ts*e_b3*(D[41] - D[23] + D[26]*(bias3 - u3));
-	P[21] = (D[21]*(Ts + e_tau) + D[27]*Ts*(u1 - u1_in))*(e_tau/Ts_e_tau2);
-	P[22] = (D[22]*(Ts + e_tau) + D[27]*Ts*(u2 - u2_in))*(e_tau/Ts_e_tau2);
-	P[23] = (D[23]*(Ts + e_tau) + D[27]*Ts*(u3 - u3_in))*(e_tau/Ts_e_tau2);
-	P[24] = D[24];
-	P[25] = D[25];
-	P[26] = D[26];
-	P[27] = D[27] + Q[9];
-	P[28] = D[28] - Ts*e_b1*(D[32] - D[29] + D[30]*(bias1 - u1));
-	P[29] = (D[29]*(Ts + e_tau) + D[31]*Ts*(u1 - u1_in))*(e_tau/Ts_e_tau2);
-	P[30] = D[30];
-	P[31] = D[31];
-	P[32] = D[32] + Q[10];
-	P[33] = D[33] - Ts*e_b2*(D[37] - D[34] + D[35]*(bias2 - u2));
-	P[34] = (D[34]*(Ts + e_tau) + D[36]*Ts*(u2 - u2_in))*(e_tau/Ts_e_tau2);
-	P[35] = D[35];
-	P[36] = D[36];
-	P[37] = D[37] + Q[11];
-	P[38] = D[38] - Ts*e_b3*(D[42] - D[39] + D[40]*(bias3 - u3));
-	P[39] = (D[39]*(Ts + e_tau) + D[41]*Ts*(u3 - u3_in))*(e_tau/Ts_e_tau2);
-	P[40] = D[40];
-	P[41] = D[41];
-	P[42] = D[42] + Q[12];
-
-
-	/********* this is the update part of the equation ***********/
-
-    float S[3] = {P[0] + s_a, P[1] + s_a, P[2] + s_a};
-
-	X[0] = w1 + P[0]*((gyro_x - w1)/S[0]);
-	X[1] = w2 + P[1]*((gyro_y - w2)/S[1]);
-	X[2] = w3 + P[2]*((gyro_z - w3)/S[2]);
-	X[3] = u1 + P[3]*((gyro_x - w1)/S[0]);
-	X[4] = u2 + P[5]*((gyro_y - w2)/S[1]);
-	X[5] = u3 + P[7]*((gyro_z - w3)/S[2]);
-	X[6] = b1 + P[9]*((gyro_x - w1)/S[0]);
-	X[7] = b2 + P[12]*((gyro_y - w2)/S[1]);
-	X[8] = b3 + P[15]*((gyro_z - w3)/S[2]);
-	X[9] = tau + P[18]*((gyro_x - w1)/S[0]) + P[19]*((gyro_y - w2)/S[1]) + P[20]*((gyro_z - w3)/S[2]);
-	X[10] = bias1 + P[28]*((gyro_x - w1)/S[0]);
-	X[11] = bias2 + P[33]*((gyro_y - w2)/S[1]);
-	X[12] = bias3 + P[38]*((gyro_z - w3)/S[2]);
-
-	// update the duplicate cache
-	for (uint32_t i = 0; i < AF_NUMP; i++)
-        D[i] = P[i];
-
-	// This is an approximation that removes some cross axis uncertainty but
-	// substantially reduces the number of calculations
-	P[0] = -D[0]*(D[0]/S[0] - 1);
-	P[1] = -D[1]*(D[1]/S[1] - 1);
-	P[2] = -D[2]*(D[2]/S[2] - 1);
-	P[3] = -D[3]*(D[0]/S[0] - 1);
-	P[4] = D[4] - D[3]*D[3]/S[0];
-	P[5] = -D[5]*(D[1]/S[1] - 1);
-	P[6] = D[6] - D[5]*D[5]/S[1];
-	P[7] = -D[7]*(D[2]/S[2] - 1);
-	P[8] = D[8] - D[7]*D[7]/S[2];
-	P[9] = -D[9]*(D[0]/S[0] - 1);
-	P[10] = D[10] - D[3]*(D[9]/S[0]);
-	P[11] = D[11] - D[9]*(D[9]/S[0]);
-	P[12] = -D[12]*(D[1]/S[1] - 1);
-	P[13] = D[13] - D[5]*(D[12]/S[1]);
-	P[14] = D[14] - D[12]*(D[12]/S[1]);
-	P[15] = -D[15]*(D[2]/S[2] - 1);
-	P[16] = D[16] - D[7]*(D[15]/S[2]);
-	P[17] = D[17] - D[15]*(D[15]/S[2]);
-	P[18] = -D[18]*(D[0]/S[0] - 1);
-	P[19] = -D[19]*(D[1]/S[1] - 1);
-	P[20] = -D[20]*(D[2]/S[2] - 1);
-	P[21] = D[21] - D[3]*(D[18]/S[0]);
-	P[22] = D[22] - D[5]*(D[19]/S[1]);
-	P[23] = D[23] - D[7]*(D[20]/S[2]);
-	P[24] = D[24] - D[9]*(D[18]/S[0]);
-	P[25] = D[25] - D[12]*(D[19]/S[1]);
-	P[26] = D[26] - D[15]*(D[20]/S[2]);
-	P[27] = D[27] - D[18]*(D[18]/S[0]) - D[19]*(D[19]/S[1]) - D[20]*(D[20]/S[2]);
-	P[28] = -D[28]*(D[0]/S[0] - 1);
-	P[29] = D[29] - D[3]*(D[28]/S[0]);
-	P[30] = D[30] - D[9]*(D[28]/S[0]);
-	P[31] = D[31] - D[18]*(D[28]/S[0]);
-	P[32] = D[32] - D[28]*(D[28]/S[0]);
-	P[33] = -D[33]*(D[1]/S[1] - 1);
-	P[34] = D[34] - D[5]*(D[33]/S[1]);
-	P[35] = D[35] - D[12]*(D[33]/S[1]);
-	P[36] = D[36] - D[19]*(D[33]/S[1]);
-	P[37] = D[37] - D[33]*(D[33]/S[1]);
-	P[38] = -D[38]*(D[2]/S[2] - 1);
-	P[39] = D[39] - D[7]*(D[38]/S[2]);
-	P[40] = D[40] - D[15]*(D[38]/S[2]);
-	P[41] = D[41] - D[20]*(D[38]/S[2]);
-	P[42] = D[42] - D[38]*(D[38]/S[2]);
-
-	// apply limits to some of the state variables
-	if (X[9] > -1.5f)
-	    X[9] = -1.5f;
-	if (X[9] < -5.5f)		/* 4ms */
-	    X[9] = -5.5f;
-	if (X[10] > 0.5f)
-	    X[10] = 0.5f;
-	if (X[10] < -0.5f)
-	    X[10] = -0.5f;
-	if (X[11] > 0.5f)
-	    X[11] = 0.5f;
-	if (X[11] < -0.5f)
-	    X[11] = -0.5f;
-	if (X[12] > 0.5f)
-	    X[12] = 0.5f;
-	if (X[12] < -0.5f)
-	    X[12] = -0.5f;
 }
 
 /**
@@ -640,66 +426,7 @@ __attribute__((always_inline)) static inline void af_predict(float X[AF_NUMX], f
  */
 static void af_init(float X[AF_NUMX], float P[AF_NUMP])
 {
-	const float q_init[AF_NUMX] = {
-		1.0f, 1.0f, 1.0f,
-		1.0f, 1.0f, 1.0f,
-		0.05f, 0.05f, 0.005f,
-		0.05f,
-		0.05f, 0.05f, 0.05f
-	};
 
-	X[0] = X[1] = X[2] = 0.0f;    // assume no rotation
-	X[3] = X[4] = X[5] = 0.0f;    // and no net torque
-	X[6] = X[7]        = 10.0f;   // medium amount of strength
-	X[8]               = 7.0f;    // yaw
-	X[9] = -4.0f;                 // and 50 ms time scale
-	X[10] = X[11] = X[12] = 0.0f; // zero bias
-
-	// P initialization
-	// Could zero this like: *P = *((float [AF_NUMP]){});
-	P[0] = q_init[0];
-	P[1] = q_init[1];
-	P[2] = q_init[2];
-	P[3] = 0.0f;
-	P[4] = q_init[3];
-	P[5] = 0.0f;
-	P[6] = q_init[4];
-	P[7] = 0.0f;
-	P[8] = q_init[5];
-	P[9] = 0.0f;
-	P[10] = 0.0f;
-	P[11] = q_init[6];
-	P[12] = 0.0f;
-	P[13] = 0.0f;
-	P[14] = q_init[7];
-	P[15] = 0.0f;
-	P[16] = 0.0f;
-	P[17] = q_init[8];
-	P[18] = 0.0f;
-	P[19] = 0.0f;
-	P[20] = 0.0f;
-	P[21] = 0.0f;
-	P[22] = 0.0f;
-	P[23] = 0.0f;
-	P[24] = 0.0f;
-	P[25] = 0.0f;
-	P[26] = 0.0f;
-	P[27] = q_init[9];
-	P[28] = 0.0f;
-	P[29] = 0.0f;
-	P[30] = 0.0f;
-	P[31] = 0.0f;
-	P[32] = q_init[10];
-	P[33] = 0.0f;
-	P[34] = 0.0f;
-	P[35] = 0.0f;
-	P[36] = 0.0f;
-	P[37] = q_init[11];
-	P[38] = 0.0f;
-	P[39] = 0.0f;
-	P[40] = 0.0f;
-	P[41] = 0.0f;
-	P[42] = q_init[12];
 }
 
 /**
